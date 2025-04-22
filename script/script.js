705606db29f2e073b023e181ccd1198a92e85d11
blastht <keitinicossavella@gmail.com>
2025-04-22 23:11:23 +0200

    Réorganisation des dossiers : suppression de l'ancien et ajout du nouveau

--- script/script.js
+++ script/script.js
@@ -1,12 +1,10 @@
-
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
+    menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
-}
-
+};
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -14,25 +12,21 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
+            navLinks.forEach(link => {
+                link.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         }
     });
 
-
-
     let header = document.querySelector('header');
-
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    menuIcon.classList.remove('bx-x');
+    menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
-
-}
+};

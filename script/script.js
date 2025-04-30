81bb1517a19b5a836a66610dbe0c1c6d966dfda0
blastht <keitinicossavella@gmail.com>
2025-04-30 08:31:09 +0200

    Changements Majeurs

--- script/script.js
+++ script/script.js
@@ -35,3 +35,46 @@ window.addEventListener('load', () => {
     const loader = document.getElementById('page-loader');
     loader.classList.add('hide');
   });
+
+  const sectionLoader = document.getElementById('section-loader');
+
+  // Sections cibles pour le chargement
+  const sectionsWithLoader = ['#portfolio', '#epreuve', '#veille'];
+
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+      const targetId = this.getAttribute('href');
+
+      // Vérifie si le lien cible une section avec loader
+      if (sectionsWithLoader.includes(targetId)) {
+        e.preventDefault(); // Empêche la navigation instantanée
+        sectionLoader.classList.add('active');
+
+        // Petite pause avant scroll pour afficher le loader
+        setTimeout(() => {
+          sectionLoader.classList.remove('active');
+          document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+          });
+        }, 2000); // durée du chargement
+      }
+    });
+  });
+
+  function openLightbox(imgElement) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+  
+    lightboxImg.src = imgElement.src; // on remplit avec l'image cliquée
+    lightbox.style.display = 'flex';
+  }
+  
+  function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+  
+    lightbox.style.display = 'none';
+    lightboxImg.src = ""; // on vide après fermeture
+  }
+  
+  
